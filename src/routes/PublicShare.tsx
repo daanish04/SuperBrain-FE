bcb585e8da9f79fcdc1c52c46c89803cf3186e76
daanish04 <108368229+daanish04@users.noreply.github.com>
2025-03-26 22:26:53 +0530

    updated public share

--- src/routes/PublicShare.tsx
+++ src/routes/PublicShare.tsx
@@ -9,7 +9,6 @@ export default function PublicShare() {
   const [user, setUser] = useState<string | null>(null);
   const [content, setContent] = useState<CardProps[]>([]);
   const { shareLink } = useParams();
-  console.log(`share link ${shareLink}`);
   const fetchContent = async (link: string) => {
     try {
       const res = await axios.get(`${API_URL}/brain/${link}`, {});
@@ -27,49 +26,68 @@ export default function PublicShare() {
   };
 
   useEffect(() => {
-    if (shareLink) {
-      fetchContent(shareLink);
-    }
+    if (!shareLink) return;
+    fetchContent(shareLink);
   }, [shareLink]);
 
   return (
     <>
       <div className="px-5 w-full min-h-screen bg-gray-100">
-        <div className="py-2 flex justify-between items-center gap-1.5">
-          <div className="flex gap-2">
-            <Print />
-            <h1 className="text-blue-700 text-2xl font-bold cursor-default">
-              SuperBrain
-            </h1>
-          </div>
-          <div className="flex gap-9 items-center">
-            <h3 className="text-lg font-medium ">{`You're exploring ${user}'s Brain`}</h3>
-          </div>
-        </div>
-        <br />
-        <br />
-        <div className="flex flex-wrap gap-4">
-          {content.length > 0 ? (
-            content.map(({ _id, title, link, description, tags }) => (
-              <Card
-                key={_id}
-                _id={_id}
-                title={title}
-                link={link}
-                description={description}
-                tags={
-                  Array.isArray(tags)
-                    ? tags.map((tag) =>
-                        typeof tag === "string" ? { title: tag } : tag
-                      )
-                    : []
-                }
-              />
-            ))
-          ) : (
-            <p className="text-gray-500 text-lg p-10">No content available</p>
-          )}
-        </div>
+        {user ? (
+          <>
+            <div className="py-2 flex justify-between items-center gap-1.5">
+              <div className="flex gap-2">
+                <Print />
+                <h1 className="text-blue-700 text-2xl font-bold cursor-default">
+                  SuperBrain
+                </h1>
+              </div>
+              <div className="flex gap-9 items-center">
+                <h3 className="text-lg font-medium ">{`You're exploring ${user}'s Brain`}</h3>
+              </div>
+            </div>
+            <br />
+            <br />
+            <div className="flex flex-wrap gap-4">
+              {content.length > 0 ? (
+                content.map(({ _id, title, link, description, tags }) => (
+                  <Card
+                    key={_id}
+                    _id={_id}
+                    title={title}
+                    link={link}
+                    description={description}
+                    tags={
+                      Array.isArray(tags)
+                        ? tags.map((tag) =>
+                            typeof tag === "string" ? { title: tag } : tag
+                          )
+                        : []
+                    }
+                  />
+                ))
+              ) : (
+                <p className="text-gray-500 text-lg p-10">
+                  No content available
+                </p>
+              )}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="py-2 flex justify-between items-center gap-1.5">
+              <div className="flex gap-2">
+                <Print />
+                <h1 className="text-blue-700 text-2xl font-bold cursor-default">
+                  SuperBrain
+                </h1>
+              </div>
+            </div>
+            <br />
+            <br />
+            <p className="text-gray-500 text-lg p-10">No Brain exists here.</p>
+          </>
+        )}
       </div>
     </>
   );
