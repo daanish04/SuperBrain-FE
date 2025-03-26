ae6457830bfc98afba5bc3de539904f29931c2ad
daanish04 <108368229+daanish04@users.noreply.github.com>
2025-03-26 09:07:53 +0530

    vercel fix3

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  base: process.env.VITE_BASE_PATH || "/SuperBrain-FE",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    // vite config
+    plugins: [tailwindcss(), react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 });
